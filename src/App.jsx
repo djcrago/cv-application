10250082afeadc561d8e16270b141c0220ef483d
Dexter Crago <jodieanddexter@gmail.com>
2024-03-02 14:21:17 -0700

    Add Skills component
    Skills component also very similar to Education and Employment
    components

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <GeneralInfo editing={editing} />
       <Education editing={editing} />
       <Employment editing={editing} />
-      {/* <Skills editing={editing} /> */}
+      <Skills editing={editing} />
       <ChangeButton editing={editing} setEditing={setEditing} />
     </>
   );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,63 @@
+import { useState } from 'react';
 import '../styles/Skills.css';
 
-export default function Skills() {}
+export default function Skills({ editing }) {
+  const [skills, setSkills] = useState([]);
+  const [nextId, setNextId] = useState(0);
+
+  function handleChange(skill, event, variable) {
+    setSkills(
+      skills.map((e) => {
+        if (e.id === skill.id) {
+          return { ...e, [variable]: event.target.value };
+        }
+        return e;
+      })
+    );
+  }
+
+  function addSkill() {
+    setNextId(nextId + 1);
+    setSkills([
+      ...skills,
+      {
+        id: nextId,
+        text: '',
+      },
+    ]);
+  }
+
+  if (editing) {
+    return (
+      <div className="skills">
+        <h2>Skills</h2>
+        {skills.map((skill) => {
+          return (
+            <form key={skill.id}>
+              <input
+                type="text"
+                placeholder="Description of skill"
+                value={skill.text}
+                onChange={(event) => handleChange(skill, event, 'text')}
+              />
+            </form>
+          );
+        })}
+        <button type="button" onClick={() => addSkill()}>
+          Add
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="skills">
+      <h2>Skills</h2>
+      <ul>
+        {skills.map((skill) => {
+          return <li key={skill.id}>{skill.text}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
